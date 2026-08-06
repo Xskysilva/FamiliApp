3d171e22324bba15a1d66df407b39dad34b1312e
Marcel Fernandes da Silva <seaskysilva@gmail.com>
2026-08-06 10:11:05 -0400

    config: Adicionar UUIDs dos usuários

--- android/app/src/main/kotlin/com/silva/familylocator/Config.kt
+++ android/app/src/main/kotlin/com/silva/familylocator/Config.kt
@@ -11,12 +11,13 @@ object Config {
     // Mapeamento de telefone para UUID
     // Copie os UUIDs do Supabase (Dashboard → Table Editor → users → column id)
     val PHONE_TO_USER_ID = mapOf(
-        "65999968208" to "ID_MARCEL",      // Alterar para UUID real
-        "6593338898" to "ID_CAMILA",       // Alterar para UUID real
-        "6596852276" to "ID_AMABILE",      // Alterar para UUID real
-        "6598136447" to "ID_AMILLE",       // Alterar para UUID real
-        "6593300-5784" to "ID_NOAH",       // Alterar para UUID real
-        "6598117-3233" to "ID_EDMARA"      // Alterar para UUID real
+    "65999968208" to "56067289-7d85-4d65-8219-37400ead8722",  // Marcel
+    "6593338898" to "49505b1c-7ae6-4b0d-933c-727adf60aa50",   // Camila
+    "6596852276" to "7259bd38-69db-4f1b-b724-bb2668ebfb1d",   // Amábile
+    "6598136447" to "d6e70ab6-be80-43b7-834b-625c082ffd9c",   // Amille
+    "6593300-5784" to "f7acc93a-5a7d-4d64-abd0-59687ecb28bd", // Noah
+    "6598117-3233" to "2c1d0609-9086-4fcc-9587-09d6aa20018f"  // Edmara
+)
     )
     
     // Encriptação
